5111ac3445e54467f2e85d7585c5e0efef713c84
Abhishek kushwah <113005763+abhishek42kushwah@users.noreply.github.com>
2024-08-04 21:09:31 +0530

    "Added About component to App, updated Marquee component styles and layout"

---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+
+function About() {
+  return (
+    <div className="w-full py-20 bg-[#CDEA68] rounded-tl-2xl rounded-tr-2xl mx-auto">
+     <div className="text-black  w-[85vw] leading-none mx-12  ">
+     <h1 className=" text-[3.8vw] font-['NeueMontreal-400' ]">
+        Ochi is a strategic partner for fast-growing tech businesses that need to <span className="underline hover:underline-none">raise funds, sell products, 
+        explain complex ideas</span>, and <span className="underline">
+        hire great people.
+        </span>
+      </h1>
+
+<div className="flex font-light  ">
+  <div>
+  What you can expect:
+  <p>
+    
+We create tailored presentations to help you persuade your colleagues, clients, or investors. Whether it’s live or digital, delivered for one or a hundred people.
+  </p>
+  <p>
+  We believe the mix of strategy and design (with a bit of coffee) is what makes your message clear, convincing, and captivating.
+  </p>
+  </div>
+</div>
+
+     </div>
+     
+
+    </div>
+  )
+}
+
+export default About
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,11 +1,11 @@
 
 function Marquee() {
   return (
-    <div className="w-full h-[50vh] bg-[#004D43] py-20 rounded-tl-xl rounded-tr-xl ">
-      <div className="text uppercase border-t-2 border-b-2 border-zinc-500 overflow-hidden whitespace-nowrap flex gap-10">
-        <h1 className="text-[17vw]  font-semibold  font -['Founders_Grotesk_X-Condensed'] leading-none -mb-32 pt-10 "> we are ochi  </h1>
-        <h1 className="text-[17vw]   font-semibold font -['Founders_Grotesk_X-Condensed'] leading-none -mb-32 pt-10"> we are ochi  </h1>
-      </div>
+    <div className="w-full h-[50vh] bg-[#004D43] rounded-tl-xl  rounded-tr-xl py-16 ">
+    <div className="border-t-2 text border-b-2  border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap  ">
+    <h1 className=" font-semibold text-[16vw] leading-none font -['Founders_Grotesk_X-Condensed'] uppercase   ">we are ochi</h1>
+    <h1 className=" font-semibold text-[16vw] leading-none font -['Founders_Grotesk_X-Condensed'] uppercase   ">we are ochi</h1>
+    </div>
     </div>
   )
 }
